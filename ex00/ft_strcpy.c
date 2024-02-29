5467fff14fb2703056f29c35e4818e3b39d02008
infectedUser68 <romansalmhofer@gmail.com>
2024-02-29 07:50:41 +0000

    yes

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src){
+	int c;
+
+	c = 0;
+	while (src[c] != '\0'){
+		dest[c] = src[c];
+		c++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
+
+int		main(void){
+	char source[] = "Source string.";
+	char destin[] = "Destination string.";
+	char *dest;
+
+	printf("BEFORE\n\tsrc: %s\n\tdes: %s\n", source, destin);
+
+    dest = ft_strcpy(destin, source);
+
+	printf("AFTER\n\tsrc: %s\n\tdes: %s\n", source, dest);
+	return (0);
+}
